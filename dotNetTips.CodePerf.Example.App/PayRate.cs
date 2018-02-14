5cc63ecae672a37a403c985ed7f3624d457e2244
David McCarter <davidmccarter@live.com>
2018-02-13 17:20:24 -0800

    Add project files.

---
+++ dotNetTips.CodePerf.Example.App/PayRate.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dotNetTips.CodePerf.Example.App
+{
+    struct PayRate : IEquatable<PayRate>
+    {
+        public PayRate(DateTime awardedOn, double rate) : this()
+        {
+            AwardedOn = awardedOn;
+            Rate = rate;
+        }
+
+        public DateTime AwardedOn { get; private set; }
+        public double Rate { get; private set; }
+
+        public override bool Equals(object obj)
+        {
+            return obj is PayRate && Equals((PayRate)obj);
+        }
+
+        public bool Equals(PayRate other)
+        {
+            return AwardedOn == other.AwardedOn &&
+                   Rate == other.Rate;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = -903919831;
+            hashCode = hashCode * -1521134295 + base.GetHashCode();
+            hashCode = hashCode * -1521134295 + AwardedOn.GetHashCode();
+            hashCode = hashCode * -1521134295 + Rate.GetHashCode();
+            return hashCode;
+        }
+
+        public static bool operator ==(PayRate rate1, PayRate rate2)
+        {
+            return rate1.Equals(rate2);
+        }
+
+        public static bool operator !=(PayRate rate1, PayRate rate2)
+        {
+            return !(rate1 == rate2);
+        }
+    }
+}
---
+++ dotNetTips.CodePerf.Example.App/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dotNetTips.CodePerf.Example.App
+{
+   internal class Person
+    {
+        private Guid _id;
+        private string _email;
+        private string _firstName;
+        private string _lastName;
+        private DateTime _bornOn;
+
+        public Person(Guid id, string email)
+        {
+            Id = id;
+            Email = email;
+        }
+
+        public Guid Id { get => _id; private set => _id = value; }
+        public string Email { get => _email; private set => _email = value; }
+        public string FirstName { get => _firstName; set => _firstName = value; }
+        public string LastName { get => _lastName; set => _lastName = value; }
+        public DateTime BornOn { get => _bornOn; set => _bornOn = value; }
+    }
+}
