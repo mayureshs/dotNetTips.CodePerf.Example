a10bc3ac8485980ccc0af82db29fd97364bd2df0
David McCarter <davidmccarter@live.com>
2018-02-13 17:25:51 -0800

    Added .NET Framework App

---
+++ dotNetTips.CodePerf.Example.FrameworkApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNetTips.CodePerf.Example.FrameworkApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
